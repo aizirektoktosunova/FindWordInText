719b9938c95ec25d84381228285a1d311aff4e2e
aizirektoktosunova <aizirektosunova853@gmail.com>
2024-03-20 16:01:08 +0300

    Initial commit

---
+++ src/main/java/com/example/findwordintextmultith1/FindWordInTextMultith1Application.java
@@ -0,0 +1,13 @@
+package com.example.findwordintextmultith1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindWordInTextMultith1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindWordInTextMultith1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/findwordintextmultith1/FindWordInTextMultithreaded.java
@@ -0,0 +1,2 @@
+package com.example.findwordintextmultith1;public class FindWordInTextMultithreaded {
+}
